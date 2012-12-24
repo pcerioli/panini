c0925be3d19517cfca3ee940e37de3cbd22d67ba
Paola Cerioli <pcerioli@gmail.com>
2012-12-23 20:21:12 -0800

    added js validation for the modal form - submitting an order

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -4,7 +4,27 @@ $(function(){
  $("#btn-order").on("click", function(event){
  		$(this).addClass('disabled');
  		$(this).html("Saving...");
-    $('#new_order').submit();
+
+
+    //$('#new_order').submit();
+
+
+		var isFormValid = true;
+    $("#new_order .required input:text").each(function(){ // Note the :text
+      if ($.trim($(this).val()).length == 0){
+        isFormValid = false;
+      }
+    });
+    if (!isFormValid) {
+    	$('.alert').html("<a class='close' data-dismiss='alert'>×</a>Please type your name!");
+    	$('.alert').show();
+    	$('#btn-order').removeClass('disabled').html("Submit");
+    } else {
+    	$('#new_order').submit();
+    }
+
+
+
   });
 
 });
